e031d2c1fa7d7a906bb302e57315265fb13fe69b
koradaPrathyusha <84893820+koradaPrathyusha@users.noreply.github.com>
2021-06-01 11:59:59 +0530

    Update SpringBootEmployeeApplication.java

--- src/main/java/com/hcl/SpringBootEmployeeApplication.java
+++ src/main/java/com/hcl/SpringBootEmployeeApplication.java
@@ -7,14 +7,20 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+ 
 
 @SpringBootApplication
 
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-public class SpringBootEmployeeApplication {
+public class SpringBootEmployeeApplication extends springBootServletInitializer{
 
 	public static void main(String[] args) {
 		 ConfigurableApplicationContext context1=SpringApplication.run(SpringBootEmployeeApplication.class, args);
 	}
+	@Override     
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {          
+		return application.sources(SpringbootFoodApplication.class);     }
 
 }
